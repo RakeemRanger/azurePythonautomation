52ffd7fa46c502d7822f1cc87339184ebfc91b58
nodebrite <rangerrakeem@gmail.com>
2025-07-11 06:20:37 -0400

    removing prefix builders sdk dependency, now using api client

--- src/autocli/autocli/core/az_rg_create.py
+++ src/autocli/autocli/core/az_rg_create.py
@@ -5,9 +5,9 @@ import time
 
 from azure.identity import DefaultAzureCredential
 
-from autocli.lib.CONSTANTS import DEV_AZURE_SUBSCRIPTION
-from ..lib.log_util import logClient
-from ..lib.azure_clients import AzureClients
+from autocli.core.lib.CONSTANTS import DEV_AZURE_SUBSCRIPTION
+from ..core.lib.log_util import logClient
+from ..core.lib.azure_clients import AzureClients
 
 class ResourceGroupCreator:
     """
